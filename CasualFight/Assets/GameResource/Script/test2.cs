d918459d9ab93062dfc94667515365f62d24703f
Abubu <108376848+chogeru@users.noreply.github.com>
2025-01-25 21:45:59 +0900

    test

--- CasualFight/Assets/GameResource/Script/test2.cs
+++ CasualFight/Assets/GameResource/Script/test2.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class test2 : MonoBehaviour
 {
+    private void Awake()
+    {
+        
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
-    private void Test()
-    {
-
-    }
+   
 }
