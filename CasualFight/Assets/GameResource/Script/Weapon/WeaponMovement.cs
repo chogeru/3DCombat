b828b8d0c3ac9385554605dadb91bb2512afb177
sogotoya <Loizacs25@gmail.com>
2025-12-17 21:29:03 +0900

    武器の動き設定（揺れ幅、傾き、距離）

--- CasualFight/Assets/GameResource/Script/Weapon/WeaponMovement.cs
+++ CasualFight/Assets/GameResource/Script/Weapon/WeaponMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static VInspector.VInspectorData;
 
 /// <summary>
 /// 武器を揺らす処理
@@ -22,12 +23,32 @@ public class WeaponMovement : MonoBehaviour
     [Header("走っているときの揺れる大きさ"), SerializeField]
     float m_DashWeaponShaking = 0.1f;
 
+    [Space]
+
+    [Header("歩いているときの刀とキャラの離れる距離"), SerializeField]
+    float m_BetweenWalk = 0.1f;
+    [Header("走っているときの刀とキャラの離れる距離"), SerializeField]
+    float m_BetweenDash = 0.15f;
+
+    [Space]
+
+    [Header("待機時の刀の角度設計"),SerializeField]
+    Vector3 m_IdleRotation = new Vector3(0, 0, 0);
+    [Header("歩いている時の刀の角度設計"), SerializeField]
+    Vector3 m_WalkRotation = new Vector3(0, 40, 0);
+    [Header("走っている時刀の角度設計"), SerializeField]
+    Vector3 m_DashRotation = new Vector3(0, 80, 0);
+
+    [Space]
+
     //最初のオブジェクトの位置を覚えておくため
     Vector3 m_WeaponDefaultPos;
+    //ベースの回転
+    Quaternion m_BaseRot = Quaternion.Euler(180, 0, 90);
 
-    [Header("プレイヤーオブジェクト"),SerializeField]
+    [Header("プレイヤーオブジェクト"), SerializeField]
     GameObject m_PlayerObj;
-    [Header("プレイヤーオブジェクト"),SerializeField]
+    [Header("プレイヤーオブジェクト"), SerializeField]
     PlayerController m_PC;
 
     private void Start()
@@ -39,34 +60,55 @@ public class WeaponMovement : MonoBehaviour
     }
     private void Update()
     {
-        if (m_PlayerObj = null)
+        if (m_PlayerObj == null || m_PC == null)
             return;
 
         //プレイヤーが移動しているかのチェック
         bool isMoving = m_PC.m_MoveInput.sqrMagnitude > 0.01f;
 
-
+        //待機状態
+        Vector3 targetPos = m_WeaponDefaultPos;
+        Vector3 targetRotOffset = Vector3.zero;
 
         if (isMoving)
         {
             //揺れを反映する変数
-            float currentSpeed=m_PC.m_IsDash?m_DashWeaponSpeed:m_WeaponSpeed;
-            float currentShaking= m_PC.m_IsDash ? m_DashWeaponShaking : m_WeaponShaking;
+            float currentSpeed = m_PC.m_IsDash ? m_DashWeaponSpeed : m_WeaponSpeed;
+            float currentShaking = m_PC.m_IsDash ? m_DashWeaponShaking : m_WeaponShaking;
 
             //揺れの計算
             float wabe = Mathf.Sin(Time.time * currentSpeed);
 
+            //移動中だけ背中から武器を離す
+            //そしてダッシュ中なら離す距離変更
+            float between = m_PC.m_IsDash ? -m_BetweenDash : -m_BetweenWalk;
+
+            //移動中だけ武器の傾きを変える
+            //そしてダッシュ中なら傾く距離変更
+            Vector3 targetRot = m_PC.m_IsDash ? m_DashRotation : m_WalkRotation;
+
+            //追加したい回転値を先に変換
+            Quaternion addRotation = Quaternion.Euler(targetRot);
+
             //座標を反映
-            Vector3 pos = m_WeaponDefaultPos;
-            pos.y += wabe * currentShaking;
+            targetPos.y += wabe * currentShaking;
+            targetPos.z += between;
+
+            //元の回転値との合成
+            Quaternion targetRotQ = addRotation * m_BaseRot;
 
             //反映
-            transform.localPosition = pos;
+            transform.localPosition = Vector3.Lerp(transform.localPosition, targetPos, Time.deltaTime * 5f);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotQ, Time.deltaTime * 5f);
         }
         else
         {
             //自然に戻す
             m_WeaponTf.localPosition = Vector3.Lerp(transform.localPosition, m_WeaponDefaultPos, Time.deltaTime * 5f);
+
+            //元の角度に戻す
+            Quaternion idleQ = Quaternion.Euler(180, 0, 90);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, idleQ, Time.deltaTime * 5f);
         }
     }
 }
