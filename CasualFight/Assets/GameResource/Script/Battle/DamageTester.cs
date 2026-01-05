94b2e5f54d4e283794db737942cb183b9b5cc5ff
sogotoya <Loizacs25@gmail.com>
2026-01-05 23:31:45 +0900

    HPバーの処理実装
    デバッグ用としてPキーで減っているかの確認可能

---
+++ CasualFight/Assets/GameResource/Script/Battle/DamageTester.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTester : MonoBehaviour
+{
+    [SerializeField] HPBarController m_HealthBarController;
+
+    private float m_CurrentHPPercent = 1.0f; // 100%
+
+    /// <summary>
+    /// インスペクターのボタンや、UIボタンから呼び出す用
+    /// </summary>
+    public void TestDamage()
+    {
+        // 20%ずつダメージを与える
+        m_CurrentHPPercent -= 0.2f;
+
+        // 0以下にならないように制限
+        if (m_CurrentHPPercent < 0) m_CurrentHPPercent = 0;
+
+        // HPバーに通知（ここであのピンクの遅延演出が走る）
+        if (m_HealthBarController != null)
+        {
+            m_HealthBarController.OnTakeDamage(m_CurrentHPPercent);
+            Debug.Log($"Damage! Current HP: {m_CurrentHPPercent * 100}%");
+        }
+    }
+
+    // スペースキー以外の「D」キーなどでもテストできるようにする場合
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TestDamage();
+        }
+    }
+}
