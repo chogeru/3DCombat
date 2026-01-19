848659852de318e1444a4c946108bceefc355271
sogotoya <Loizacs25@gmail.com>
2026-01-19 20:21:58 +0900

    ミニマップの視野角機能追加、それに伴う画像オブジェクト追加＆カメラの向いている方向に向いたりズームによってサイズが変わる機能MinimapPlayerArrowSyncを追加

---
+++ CasualFight/Assets/GameResource/Script/Player/MinimapPlayerArrowSync.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 矢印をプレイヤーの向きに同期させるスクリプト
+/// </summary>
+public class MinimapPlayerArrowSync : MonoBehaviour
+{
+    [Header("プレイヤーオブジェクト"), SerializeField]
+    Camera m_MainCamera;
+    [Header("視野角の値をScaleに変換する際の倍率"), SerializeField]
+    float m_WidthMultiplier = 0.015f;
+
+    private void Start()
+    {
+        //アタッチされていない場合
+        if(m_MainCamera==null)
+        m_MainCamera = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        //カメラ本体のY軸取得
+        float cameraYAngle=m_MainCamera.transform.eulerAngles.y;
+
+        //X軸を平面にし、Y軸回転方向、Z軸は0で固定化し、プレイヤーカメラの向きと同じにする
+        transform.rotation=Quaternion.Euler(transform.rotation.x, cameraYAngle, 0);
+
+        //カメラの視野角を取得
+        float currentFov = m_MainCamera.fieldOfView;
+
+        //FOVが大きくなると、扇形も横に広がるように計算
+        float targetWidth = currentFov * m_WidthMultiplier;
+
+        //現在のスケール取得
+        Vector3 objScale = transform.localScale;
+
+        //X幅のみ書き換え
+        objScale.x = targetWidth;
+
+        //適応
+        transform.localScale = objScale;
+    }
+}
